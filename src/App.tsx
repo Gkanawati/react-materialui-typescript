a025376ca42ec0c9d1167c6f88e1f4b93b875786
Gkanawati <gabrielkanawati3@gmail.com>
2022-08-23 11:39:21 -0300

    feat: Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export default function App() {
   return (
-    <div className="App">
-      Teste
-    </div>
-  )
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
 }
