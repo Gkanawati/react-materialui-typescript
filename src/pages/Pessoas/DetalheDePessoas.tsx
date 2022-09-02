a85d31646b890729147b305fc851c10fac03114f
Gkanawati <gabrielkanawati3@gmail.com>
2022-09-02 10:35:51 -0300

    feat: Usando Layout Grid no form com Material UI

--- src/pages/Pessoas/DetalheDePessoas.tsx
+++ src/pages/Pessoas/DetalheDePessoas.tsx
@@ -1,4 +1,4 @@
-import { LinearProgress } from "@mui/material";
+import { LinearProgress, Box, Paper, Grid, Typography } from "@mui/material";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { LayoutBase } from "../../shared/layouts";
@@ -94,12 +94,58 @@ export const DetalheDePessoas: React.FC = () => {
         />
       }
     >
-      {isLoading && <LinearProgress variant="indeterminate" />}
-
       <Form ref={formRef} onSubmit={handleSave}>
-        <VTextField placeholder="Nome Completo" name="nomeCompleto" />
-        <VTextField placeholder="Email" name="email" />
-        <VTextField placeholder="Cidade" name="cidadeId" />
+        <Box
+          margin={1}
+          display="flex"
+          flexDirection="column"
+          component={Paper}
+          variant="outlined"
+        >
+          <Grid container direction="column" padding={2} spacing={2}>
+            {isLoading && (
+              <Grid item>
+                <LinearProgress variant="indeterminate" />
+              </Grid>
+            )}
+
+            <Grid item>
+              <Typography variant="h6">Geral</Typography>
+            </Grid>
+
+            <Grid container item direction="row" spacing={2}>
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+                <VTextField
+                  fullWidth
+                  label="Nome Completo"
+                  name="nomeCompleto"
+                  disabled={isLoading}
+                  onChange={(e) => setNome(e.target.value)}
+                />
+              </Grid>
+            </Grid>
+            <Grid container item direction="row" spacing={2}>
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+                <VTextField
+                  fullWidth
+                  label="Email"
+                  name="email"
+                  disabled={isLoading}
+                />
+              </Grid>
+            </Grid>
+            <Grid container item direction="row" spacing={2}>
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+                <VTextField
+                  fullWidth
+                  label="Cidade"
+                  name="cidadeId"
+                  disabled={isLoading}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
       </Form>
     </LayoutBase>
   );
