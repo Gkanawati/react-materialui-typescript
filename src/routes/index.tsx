5449d0c31f9ca1e42ecce2c0ed69c014f9e5db63
Gkanawati <gabrielkanawati3@gmail.com>
2022-09-05 12:05:39 -0300

    Publish Project

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
-import { useEffect } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useDrawerContext } from "../shared/contexts";
+import { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useDrawerContext } from '../shared/contexts';
 import {
   Dashboard,
   DetalheDeCidades,
   DetalheDePessoas,
   ListagemDeCidades,
   ListagemDePessoas,
-} from "../pages";
+} from '../pages';
 
 export const AppRoutes = () => {
   const { setDrawerOptions } = useDrawerContext();
@@ -15,34 +15,34 @@ export const AppRoutes = () => {
   useEffect(() => {
     setDrawerOptions([
       {
-        icon: "home",
-        label: "Página Inicial",
-        path: "/pagina-inicial",
+        icon: 'home',
+        label: 'Página Inicial',
+        path: '/pagina-inicial',
       },
       {
-        icon: "apartment",
-        label: "Cidades",
-        path: "/cidades",
+        icon: 'apartment',
+        label: 'Cidades',
+        path: '/cidades',
       },
       {
-        icon: "people",
-        label: "Pessoas",
-        path: "/pessoas",
+        icon: 'people',
+        label: 'Pessoas',
+        path: '/pessoas',
       },
     ]);
   }, []);
 
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Dashboard />} />
+      <Route path='/pagina-inicial' element={<Dashboard />} />
 
-      <Route path="/pessoas" element={<ListagemDePessoas />} />
-      <Route path="/pessoas/detalhe/:id" element={<DetalheDePessoas />} />
+      <Route path='/pessoas' element={<ListagemDePessoas />} />
+      <Route path='/pessoas/detalhe/:id' element={<DetalheDePessoas />} />
 
-      <Route path="/cidades" element={<ListagemDeCidades />} />
-      <Route path="/cidades/detalhe/:id" element={<DetalheDeCidades />} />
+      <Route path='/cidades' element={<ListagemDeCidades />} />
+      <Route path='/cidades/detalhe/:id' element={<DetalheDeCidades />} />
 
-      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+      <Route path='*' element={<Navigate to='/pagina-inicial' />} />
     </Routes>
   );
 };
